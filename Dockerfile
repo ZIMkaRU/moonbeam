8b9b6aca5fb795c9ed3a2f3441454c4077491b14
Vladimir Voronkov <vsvoronkov@gmail.com>
2019-10-15 14:43:07 +0300

    Fix deploy configs

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 FROM node:10.16.3-alpine
 
-COPY package.json init.configs.sh ./
-COPY config/moonbeam.redis.conf.json.example \
-  config/moonbeam.mongo.conf.json.example \
-  config/mongo.pubtrades.conf.json.example \
-  config/moonbeam.conf.json.example \
-  config/
+COPY package.json ./
 
 RUN apk add --no-cache \
   git \
   openssh \
   bash \
-  &&npm install \
-  &&./init.configs.sh
+  &&npm install
 
 CMD ["node", "worker.js"]
--- init.configs.sh
+++ init.configs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source .env
+
+set -x
+
 cp config/moonbeam.redis.conf.json.example config/moonbeam.redis.conf.json
 cp config/moonbeam.mongo.conf.json.example config/moonbeam.mongo.conf.json
 cp config/moonbeam.conf.json.example config/moonbeam.conf.json
@@ -8,5 +12,7 @@ cp config/mongo.pubtrades.conf.json.example config/mongo.pubtrades.conf.json
 sed -i -e "s/\"host\": .*,/\"host\": \"redis\",/g" config/moonbeam.redis.conf.json
 sed -i -e "s/\"mongoUrl\": .*,/\"mongoUrl\": \"mongodb:\/\/mongo\",/g" config/moonbeam.mongo.conf.json
 sed -i -e "s/\"mongoUrl\": .*,/\"mongoUrl\": \"mongodb:\/\/mongo\",/g" config/mongo.pubtrades.conf.json
+sed -i -e "s/\"keyProvider\": .*,/\"keyProvider\": \[\"$FAUCET_KEY_PROVIDER\"\],/g" config/moonbeam.conf.json
 
 echo "Configs are ready"
+ls -la config
